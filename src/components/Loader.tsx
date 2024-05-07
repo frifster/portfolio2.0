14d35005dad56d447c5393147d0c15a93289951e
Eugene Avila <frifster2014@gmail.com>
2024-05-07 23:40:57 +0800

    Update Loader component style
    The styling of the Loader component was revised. The dimensions and color of the spinning element were changed and made more attractive, while unnecessary nested elements and classes were removed for a cleaner implementation.

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,11 +3,12 @@ import {Html} from "@react-three/drei";
 const Loader = () => {
     return (
         <Html>
-        <div className="w-full h-screen flex items-center justify-center">
-            <div className="w-10 h-10 border-2 border-gray-300 rounded-full animate-spin"></div>
-        </div>
+            <div className='flex justify-center items-center'>
+                <div
+                    className='w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin'></div>
+            </div>
         </Html>
-    )
-}
+    );
+};
 
-export default Loader
+export default Loader;
