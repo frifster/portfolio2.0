2cce0231b531a001f27f37157249e170ccc25031
Eugene Avila <frifster2014@gmail.com>
2024-05-07 16:48:04 +0800

    Added color, fontFamily, boxShadow to tailwind.config and modified App.tsx
    The tailwind.config.js is extended with additional color options, fontFamily choices (Work Sans, and Poppins), and a boxShadow style for the card. In App.tsx, the default content is replaced with a

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import './App.css'
-
 function App() {
   return (
-      <h1 className="text-3xl font-bold underline">
-          Hello world!
-      </h1>
+      <main>
+          <h1>Portfolio 2.0</h1>
+      </main>
   )
 }
 
