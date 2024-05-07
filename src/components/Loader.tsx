cf2675419a8d637c0a74a3c515299ddde73f2415
Eugene Avila <frifster2014@gmail.com>
2024-05-07 17:44:12 +0800

    Add new dependencies and update pnpm-lock.yaml

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,8 +1,12 @@
+import {Html} from "@react-three/drei";
+
 const Loader = () => {
     return (
+        <Html>
         <div className="w-full h-screen flex items-center justify-center">
             <div className="w-10 h-10 border-2 border-gray-300 rounded-full animate-spin"></div>
         </div>
+        </Html>
     )
 }
 
