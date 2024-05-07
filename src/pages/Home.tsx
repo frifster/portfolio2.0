f706484a89f02d962a71640acc17a013965cf0fa
Eugene Avila <frifster2014@gmail.com>
2024-05-07 18:21:07 +0800

    Add 3D Island model and update dependencies
    The commit introduces a 3D Island model to the Home page and updates package dependencies to incorporate the latest version of `react-spring` library. Optimizations for different screen sizes have also been provided for the new model. Additionally, the Vite config has been updated to include `glb` and `gltf` file types.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,20 @@
-import {Canvas} from "@react-three/fiber";
+import {Canvas, Vector3} from "@react-three/fiber";
 import {Suspense} from "react";
 import Loader from "../components/Loader.tsx";
+import Island from "../models/Island.tsx";
 
 const HomePage = () => {
+    const adjustIslandForScreenSize = () => {
+        const screenPosition: Vector3 = [0, -6.5, -43];
+        const screenScale: Vector3 = window.innerWidth < 768 ? [0.9, 0.9, 0.9] : [1, 1, 1];
+        return {
+            screenScale,
+            screenPosition
+        }
+    }
+
+    const {screenScale, screenPosition} = adjustIslandForScreenSize();
+
     return (
         <section className="w-full h-screen relative">
             <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
@@ -17,11 +29,10 @@ const HomePage = () => {
             >
                 <Suspense fallback={<Loader/>}>
                     <ambientLight/>
-                    <pointLight position={[10, 10, 10]}/>
-                    <mesh>
-                        <boxGeometry args={[1, 1, 1]}/>
-                        <meshStandardMaterial color="hotpink"/>
-                    </mesh>
+                    <Island
+                        position={screenPosition}
+                        scale={screenScale}
+                    />
                 </Suspense>
             </Canvas>
         </section>
