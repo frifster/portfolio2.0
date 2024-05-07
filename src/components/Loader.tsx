606e94f09dd4ce90afb27698133ef35c8495a27e
Eugene Avila <frifster2014@gmail.com>
2024-05-07 17:32:12 +0800

    Add @react-three/fiber dependency and implement basic scene in Home page
    A new dependency, @react-three/fiber, has been added, and it is used to implement a basic 3D scene in the Home page. To handle 3D object loading, a new Loader component has also been created and used for suspension fallback inside the Canvas module of Home page. This commit lays the groundwork for further 3D implementation.

---
+++ src/components/Loader.tsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+    return (
+        <div className="w-full h-screen flex items-center justify-center">
+            <div className="w-10 h-10 border-2 border-gray-300 rounded-full animate-spin"></div>
+        </div>
+    )
+}
+
+export default Loader
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,29 @@
+import {Canvas} from "@react-three/fiber";
+import {Suspense} from "react";
+import Loader from "../components/Loader.tsx";
+
 const HomePage = () => {
     return (
         <section className="w-full h-screen relative">
             <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
                 Pop UP
             </div>
+            <Canvas
+                className="w-full h-screen bg-transparent"
+                camera={{
+                    near: 0.1,
+                    far: 1000,
+                }}
+            >
+                <Suspense fallback={<Loader/>}>
+                    <ambientLight/>
+                    <pointLight position={[10, 10, 10]}/>
+                    <mesh>
+                        <boxGeometry args={[1, 1, 1]}/>
+                        <meshStandardMaterial color="hotpink"/>
+                    </mesh>
+                </Suspense>
+            </Canvas>
         </section>
     )
 }
